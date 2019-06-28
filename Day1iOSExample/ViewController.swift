dcf8df5edc3e94063b0e2dea2c2d3e3b6bac0223
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-06-28 01:30:21 -0400

    [Neeraj]  Added func to Add int from two text fields and display the sum on a label

--- Day1iOSExample/ViewController.swift
+++ Day1iOSExample/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblMessage: UILabel!
+    
+    @IBOutlet weak var txtMessage: UITextField!
+    @IBOutlet weak var txtMessage2: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func btnAdd(_ sender: UIButton) {
+        let x = Int(self.txtMessage.text!)
+        let y = Int(self.txtMessage2.text!)
+        if x != nil{
+            if y != nil{
+                self.lblMessage.text = String(x! + y!)
+            }else{
+                self.lblMessage.text = "Please enter both as numbers"
+                }
+        }else{
+            self.lblMessage.text = "Please enter both as numbers"
+        }
+    }
 }
 
